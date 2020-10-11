ea2bf542649d707f96091e93e2153afb6c94c289
Jakub <jakubjachowski@icloud.com>
2020-10-11 16:50:51 +0200

    routing to collection category, changed fake data data structure, added collection page

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CollectionPreview from '../collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 import './collections-overview.styles.scss';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
@@ -18,7 +18,7 @@ const CollectionsOverview = ({ collections }) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
